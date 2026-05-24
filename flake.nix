89da1c903991974d85e6d2c8a542059fe77165a3
Ethan Marshall <113003659+Ethan-Marshall@users.noreply.github.com>
2026-05-23 17:56:48 -0700

    Add Noctalia to flake

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
   outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.FracturedStack = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
