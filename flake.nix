1ac80221185b62088ffc579dd09f9ff56cd31c4f
Ethan Marshall <113003659+Ethan-Marshall@users.noreply.github.com>
2026-05-23 17:31:56 -0700

    Add Noctalia to flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,25 @@
 {
   description = "FracturedStack NixOS configuration";
 
+
+
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
+
+
 
   outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.FracturedStack = nixpkgs.lib.nixosSystem {
@@ -15,7 +27,11 @@
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
+        ./noctalia.nix
       ];
     };
   };
+
+
+
 }
