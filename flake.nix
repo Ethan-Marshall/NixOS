a8d51db0f9eb064f14967d7db7301554cbf16838
Ethan Marshall <113003659+Ethan-Marshall@users.noreply.github.com>
2026-05-23 17:52:09 -0700

    Add Noctalia to flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "FracturedStack NixOS configuration";
 
+  
+  nixConfig = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+  };
 
 
 
